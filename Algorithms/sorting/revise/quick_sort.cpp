bbce168c640d30496b13e28b0732a9ba512a7eb3
Devanshu Garg <officialdevanshu05@gmail.com>
2025-06-09 14:48:17 +0530

    Quick sort Complete

--- Algorithms/sorting/revise/quick_sort.cpp
+++ Algorithms/sorting/revise/quick_sort.cpp
@@ -1,9 +1,39 @@
+// pick the pivot and place it in its correct place in the sorted array
+// smaller on the left, larger on the right
 #include<iostream>
 #include<vector>
 using namespace std;
 
-void quickSort(vector<int>& arr){
+int qS(vector<int>& arr, int low, int high){
 
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+
+    while(i<j){
+
+        while(arr[i]<=pivot && i<=high-1){
+            i++;
+        }
+        while(arr[j]>pivot && j>=low+1){
+            j--;
+        }
+        if(i<j){
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+
+void quickSort(vector<int>& arr, int low, int high){
+    
+    if(low<high){
+        int pIndex = qS(arr, low, high);
+        quickSort(arr, low, pIndex-1);
+        quickSort(arr, pIndex+1, high);
+    }
+    
 }
 
 int main(){
@@ -18,7 +48,7 @@ int main(){
         arr.push_back(x);
     }
 
-    quickSort(arr);
+    quickSort(arr, 0, n-1);
 
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
